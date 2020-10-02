0030c1aacd0542452b4a621f9789db27a00b7cf3
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-10-02 22:54:17 +0800

    while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,34 +8,16 @@
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2020
 #------------------------------------------------------------------------
-
 file_object= open("./data/raw/Sara.txt", "r")
  
  #Read contents of file into a list
-line_list = file_object.readlines()
+lineString = file_object.readline()
 
-#Close the file
-file_object.close()
-
-#Pretend we read one line of data from the file
-lineString = line_list[2]
-
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara
-#print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-#Iterate through all lines in the the lineList
-for lineString in line_list:
-    if lineString[0] in ("#","u"): continue
-
+while lineString:
+    if lineString[0] in ("#","u"): 
+        # Move to the next line
+        lineString = file_object.readline()
+        continue
     #Split the string into a list of data items
     lineData = lineString.split()
     
@@ -46,8 +28,14 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
     
-    #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    
+    # Move to the next line
+    lineString = file_object.readline()
+      
+# Close the file
+file_object.close()
+
 
 
 
