7fc4bbde5b99b3d9f3db881fe1f8fa1441fbbc12
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-10-03 15:24:45 +0800

    built dictionary

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -1,45 +1,44 @@
-#-----------------------------------------------------------------------
-# ARGOSTrackingTool.py
-#
-# Description: Reads in an ARGOS tracking data file and allows
-#   the user to view the location of the turtle for a specified
-#   date entered via user input.
 #
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2020
+#-----------------------------------------------------------------------
+#lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
 #------------------------------------------------------------------------
-file_object= open("./data/raw/Sara.txt", "r")
- 
- #Read contents of file into a list
-lineString = file_object.readline()
 
-while lineString:
-    if lineString[0] in ("#","u"): 
-        # Move to the next line
-        lineString = file_object.readline()
-        continue
+file_object= open("./data/raw/Sara.txt", "r")
+
+ #Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Create two empty dictionary object
+date_dict = {}
+coord_dict = {}
+
+
+
+#Print the location of sara
+#print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+#Iterate through all lines in the the lineList
+for lineString in line_list:
+    if lineString[0] in ("#","u"): continue
+
     #Split the string into a list of data items
     lineData = lineString.split()
-    
+
     #Extract items in list into variables
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
     obs_lat = lineData[6]
     obs_lon = lineData[7]
-    
+
+    #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-    
-    # Move to the next line
-    lineString = file_object.readline()
-      
-# Close the file
-file_object.close()
-
-
-
-
-
+    date_dict[record_id] = obs_date
+    coord_dict[record_id] = (obs_lat,obs_lon)
 
 
 
